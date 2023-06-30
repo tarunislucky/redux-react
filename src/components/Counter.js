9f553122fae9745c61deef4a15b703bee4715bde
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-06-30 08:21:56 +0530

    dispatch actions from component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   // Subscription is abstracted inside useSelector
   const counter = useSelector(state => {
     return state.counter;
   })
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  }
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  }
   const toggleCounterHandler = () => {
 
   };
@@ -14,6 +21,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
